e91eac53d26ee36e421d9bc47a70fcddd5e4c05d
Duna Alexandru <147441331+AlexDuna@users.noreply.github.com>
2024-04-16 02:24:00 +0300

    Aplicatie Fluturas Salariu
    Aplicatia calculeaza CFS,CAS,CASS,IMPOZIT si altele pentru angajat, dar si pentru angajator. Tot ce trebuie introdus este salariul brut.

---
+++ Practice 1/Aplicatie pentru un fluturas de salar/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Aplicatie_pentru_un_fluturas_de_salar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SalariuBox = new System.Windows.Forms.TextBox();
+            this.SalariuButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.CFS = new System.Windows.Forms.Label();
+            this.CAS = new System.Windows.Forms.Label();
+            this.CASS = new System.Windows.Forms.Label();
+            this.Impozit = new System.Windows.Forms.Label();
+            this.CAS_angajator = new System.Windows.Forms.Label();
+            this.CFS_angajator = new System.Windows.Forms.Label();
+            this.CFCI = new System.Windows.Forms.Label();
+            this.CFGPCS = new System.Windows.Forms.Label();
+            this.CASS_angajator = new System.Windows.Forms.Label();
+            this.CFAMBP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salariu Brut";
+            // 
+            // SalariuBox
+            // 
+            this.SalariuBox.Location = new System.Drawing.Point(185, 13);
+            this.SalariuBox.Name = "SalariuBox";
+            this.SalariuBox.Size = new System.Drawing.Size(157, 22);
+            this.SalariuBox.TabIndex = 1;
+            // 
+            // SalariuButton
+            // 
+            this.SalariuButton.Location = new System.Drawing.Point(407, 13);
+            this.SalariuButton.Name = "SalariuButton";
+            this.SalariuButton.Size = new System.Drawing.Size(151, 35);
+            this.SalariuButton.TabIndex = 2;
+            this.SalariuButton.Text = "Calculeaza";
+            this.SalariuButton.UseVisualStyleBackColor = true;
+            this.SalariuButton.Click += new System.EventHandler(this.SalariuButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ANGAJAT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Contributia la Fondul de Somaj (CFS)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contributia la Asigurarile Sociale (CAS)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(323, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Contributia la Asigurarile Sociale de Sanatate (CASS)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Impozit pe Venit";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(29, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "ANGAJATOR";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 249);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(200, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Contributia la Asigurarile Sociale";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 276);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(189, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Contributia la Fondul de Somaj";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 303);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(284, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Contributia la fondul de Concedii si Indemnizatii";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(30, 329);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(376, 16);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Contributia la fondul de Garantare al Platii Creantelor Salariale ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 355);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(265, 16);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Contributia la Asigurari Sociale de Sanatate";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(30, 383);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(393, 16);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Contributia la Fondul de Acctidente de Munca si Boli Profesionale";
+            // 
+            // CFS
+            // 
+            this.CFS.AutoSize = true;
+            this.CFS.Location = new System.Drawing.Point(461, 88);
+            this.CFS.Name = "CFS";
+            this.CFS.Size = new System.Drawing.Size(16, 16);
+            this.CFS.TabIndex = 15;
+            this.CFS.Text = "...";
+            // 
+            // CAS
+            // 
+            this.CAS.AutoSize = true;
+            this.CAS.Location = new System.Drawing.Point(461, 115);
+            this.CAS.Name = "CAS";
+            this.CAS.Size = new System.Drawing.Size(16, 16);
+            this.CAS.TabIndex = 16;
+            this.CAS.Text = "...";
+            // 
+            // CASS
+            // 
+            this.CASS.AutoSize = true;
+            this.CASS.Location = new System.Drawing.Point(461, 140);
+            this.CASS.Name = "CASS";
+            this.CASS.Size = new System.Drawing.Size(16, 16);
+            this.CASS.TabIndex = 17;
+            this.CASS.Text = "...";
+            // 
+            // Impozit
+            // 
+            this.Impozit.AutoSize = true;
+            this.Impozit.Location = new System.Drawing.Point(461, 165);
+            this.Impozit.Name = "Impozit";
+            this.Impozit.Size = new System.Drawing.Size(16, 16);
+            this.Impozit.TabIndex = 18;
+            this.Impozit.Text = "...";
+            // 
+            // CAS_angajator
+            // 
+            this.CAS_angajator.AutoSize = true;
+            this.CAS_angajator.Location = new System.Drawing.Point(461, 238);
+            this.CAS_angajator.Name = "CAS_angajator";
+            this.CAS_angajator.Size = new System.Drawing.Size(16, 16);
+            this.CAS_angajator.TabIndex = 19;
+            this.CAS_angajator.Text = "...";
+            // 
+            // CFS_angajator
+            // 
+            this.CFS_angajator.AutoSize = true;
+            this.CFS_angajator.Location = new System.Drawing.Point(461, 267);
+            this.CFS_angajator.Name = "CFS_angajator";
+            this.CFS_angajator.Size = new System.Drawing.Size(16, 16);
+            this.CFS_angajator.TabIndex = 20;
+            this.CFS_angajator.Text = "...";
+            // 
+            // CFCI
+            // 
+            this.CFCI.AutoSize = true;
+            this.CFCI.Location = new System.Drawing.Point(461, 303);
+            this.CFCI.Name = "CFCI";
+            this.CFCI.Size = new System.Drawing.Size(16, 16);
+            this.CFCI.TabIndex = 21;
+            this.CFCI.Text = "...";
+            // 
+            // CFGPCS
+            // 
+            this.CFGPCS.AutoSize = true;
+            this.CFGPCS.Location = new System.Drawing.Point(461, 329);
+            this.CFGPCS.Name = "CFGPCS";
+            this.CFGPCS.Size = new System.Drawing.Size(16, 16);
+            this.CFGPCS.TabIndex = 22;
+            this.CFGPCS.Text = "...";
+            // 
+            // CASS_angajator
+            // 
+            this.CASS_angajator.AutoSize = true;
+            this.CASS_angajator.Location = new System.Drawing.Point(461, 355);
+            this.CASS_angajator.Name = "CASS_angajator";
+            this.CASS_angajator.Size = new System.Drawing.Size(16, 16);
+            this.CASS_angajator.TabIndex = 23;
+            this.CASS_angajator.Text = "...";
+            // 
+            // CFAMBP
+            // 
+            this.CFAMBP.AutoSize = true;
+            this.CFAMBP.Location = new System.Drawing.Point(461, 383);
+            this.CFAMBP.Name = "CFAMBP";
+            this.CFAMBP.Size = new System.Drawing.Size(16, 16);
+            this.CFAMBP.TabIndex = 24;
+            this.CFAMBP.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CFAMBP);
+            this.Controls.Add(this.CASS_angajator);
+            this.Controls.Add(this.CFGPCS);
+            this.Controls.Add(this.CFCI);
+            this.Controls.Add(this.CFS_angajator);
+            this.Controls.Add(this.CAS_angajator);
+            this.Controls.Add(this.Impozit);
+            this.Controls.Add(this.CASS);
+            this.Controls.Add(this.CAS);
+            this.Controls.Add(this.CFS);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SalariuButton);
+            this.Controls.Add(this.SalariuBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SalariuBox;
+        private System.Windows.Forms.Button SalariuButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label CFS;
+        private System.Windows.Forms.Label CAS;
+        private System.Windows.Forms.Label CASS;
+        private System.Windows.Forms.Label Impozit;
+        private System.Windows.Forms.Label CAS_angajator;
+        private System.Windows.Forms.Label CFS_angajator;
+        private System.Windows.Forms.Label CFCI;
+        private System.Windows.Forms.Label CFGPCS;
+        private System.Windows.Forms.Label CASS_angajator;
+        private System.Windows.Forms.Label CFAMBP;
+    }
+}
+
---
+++ Practice 1/Aplicatie pentru un fluturas de salar/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicatie_pentru_un_fluturas_de_salar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SalariuButton_Click(object sender, EventArgs e)
+        {
+            double salariu = double.Parse(SalariuBox.Text);
+            double cfs = (salariu / 1000) * 5;
+            double cas = (salariu / 1000) * 105;
+            double cass = (salariu / 1000) * 55;
+            double impozit =((salariu - cfs - cas - cass - 180) / 1000) * 160;
+            double cas_angajator = (salariu / 1000) * 208;
+            double cfs_angajator = (salariu / 1000) * 50;
+            double cfci = (salariu / 1000) * 85;
+            double cfgpcs = (salariu / 10000) * 25;
+            double cass_angajator = (salariu / 1000) * 52;
+            double cfambp = (salariu / 1000) * 4;
+
+            CFS.Text = cfs.ToString();
+            CAS.Text = cas.ToString();
+            CASS.Text = cass.ToString();
+            Impozit.Text = impozit.ToString();
+            CAS_angajator.Text = cas_angajator.ToString();
+            CFS_angajator.Text = cfs_angajator.ToString();
+            CFCI.Text = cfci.ToString();
+            CFGPCS.Text = cfgpcs.ToString();
+            CASS_angajator.Text = cass_angajator.ToString();
+            CFAMBP.Text = cfambp.ToString();
+        }
+    }
+}
